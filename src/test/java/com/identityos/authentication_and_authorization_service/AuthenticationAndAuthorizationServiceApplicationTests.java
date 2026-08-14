3f6d0ad17783c0a986ddbc869f00227d266cef89
dajinkya1099 <dajinkya1099@gmail.com>
2026-08-14 17:06:57 +0530

    Initial commit: authentication and authorization service

---
+++ src/test/java/com/identityos/authentication_and_authorization_service/AuthenticationAndAuthorizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.identityos.authentication_and_authorization_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationAndAuthorizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
